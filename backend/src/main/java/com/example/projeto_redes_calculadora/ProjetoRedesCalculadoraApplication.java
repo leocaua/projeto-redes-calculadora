1594b6785c48e0ece379b6b7be5527a0f8040949
Leonardo Cauã <leonardocauagsilva@icloud.com>
2026-06-01 17:29:23 -0300

    chore: setup inicial do projeto Spring Boot

--- backend/src/main/java/com/example/projeto_redes_calculadora/ProjetoRedesCalculadoraApplication.java
+++ backend/src/main/java/com/example/projeto_redes_calculadora/ProjetoRedesCalculadoraApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.projeto_redes_calculadora;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ProjetoRedesCalculadoraApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ProjetoRedesCalculadoraApplication.class, args);
 	}
 
 }
--- backend/src/test/java/com/example/projeto_redes_calculadora/ProjetoRedesCalculadoraApplicationTests.java
+++ backend/src/test/java/com/example/projeto_redes_calculadora/ProjetoRedesCalculadoraApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.projeto_redes_calculadora;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ProjetoRedesCalculadoraApplicationTests {
 
 	@Test
 	void contextLoads() {
