7429ebd1e66718b565e799d85b4f4706cad55d08
Leonardo Cauã <leonardocauagsilva@icloud.com>
2026-06-02 21:51:49 -0300

    Ajustes no design da interface

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,6 +3,6 @@ import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient() // Isso é o que permite o Angular conversar com o Java
+    provideHttpClient()
   ]
 };
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -24,7 +24,6 @@ interface RedeDTO {
 })
 export class App {
 
-  // Alteração 1: Começam vazios
   ipDigitado: string = '';
   mascaraDigitada: string = '';
 
@@ -34,14 +33,12 @@ export class App {
   constructor(private http: HttpClient) {}
 
   calcularRede() {
-    // Validação para não enviar nada vazio pro Java
     if (!this.ipDigitado || !this.mascaraDigitada) {
       this.erroMsg = 'Por favor, preencha os dois campos!';
       return;
     }
 
     this.erroMsg = '';
-    // Alteração 2: A URL agora manda "mascara=" (encodeURIComponent protege a barra '/' na URL)
     const url = `http://localhost:8080/api/calculadora/calcular?ip=${this.ipDigitado}&mascara=${encodeURIComponent(this.mascaraDigitada)}`;
 
     this.http.get<RedeDTO>(url).subscribe({
