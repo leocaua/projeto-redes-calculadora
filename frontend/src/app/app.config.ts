b0c09552230e3f876d9da24696dfb6b2681206ba
Leonardo Cauã <leonardocauagsilva@icloud.com>
2026-06-02 21:36:34 -0300

    criação interface e integração do front-end com a API Java

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,9 +1,8 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    
+    provideHttpClient() // Isso é o que permite o Angular conversar com o Java
   ]
 };
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,58 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+interface RedeDTO {
+  ipInformado: string;
+  mascaraSubrede: string;
+  prefixoCidr: string;
+  enderecoRede: string;
+  enderecoBroadcast: string;
+  primeiroIpValido: string;
+  ultimoIpValido: string;
+  quantidadeHosts: number;
+  classeIp: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('frontend');
+
+  // Alteração 1: Começam vazios
+  ipDigitado: string = '';
+  mascaraDigitada: string = '';
+
+  resultadoDaAPI: RedeDTO | null = null;
+  erroMsg: string = '';
+
+  constructor(private http: HttpClient) {}
+
+  calcularRede() {
+    // Validação para não enviar nada vazio pro Java
+    if (!this.ipDigitado || !this.mascaraDigitada) {
+      this.erroMsg = 'Por favor, preencha os dois campos!';
+      return;
+    }
+
+    this.erroMsg = '';
+    // Alteração 2: A URL agora manda "mascara=" (encodeURIComponent protege a barra '/' na URL)
+    const url = `http://localhost:8080/api/calculadora/calcular?ip=${this.ipDigitado}&mascara=${encodeURIComponent(this.mascaraDigitada)}`;
+
+    this.http.get<RedeDTO>(url).subscribe({
+      next: (dados) => {
+        this.resultadoDaAPI = dados;
+      },
+      error: (erro) => {
+        console.error(erro);
+        this.erroMsg = 'Valores inválidos ou servidor offline. Verifique o formato digitado.';
+        this.resultadoDaAPI = null;
+      }
+    });
+  }
 }
