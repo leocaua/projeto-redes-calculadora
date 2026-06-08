789b4cd4430c06d19202c79e28af462f4016e3ae
Leonardo Cauã <leonardocauagsilva@icloud.com>
2026-06-08 10:11:47 -0300

    Atualização para listar todos os blocos de sub-redes e não apenas o ultimo

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -3,16 +3,16 @@ import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+interface BlocoRedeDTO {
+  numero: number; enderecoRede: string; enderecoBroadcast: string;
+  primeiroIpValido: string; ultimoIpValido: string; quantidadeHosts: number; contemIpInformado: boolean;
+}
+
 interface RedeDTO {
-  ipInformado: string;
-  mascaraSubrede: string;
-  prefixoCidr: string;
-  enderecoRede: string;
-  enderecoBroadcast: string;
-  primeiroIpValido: string;
-  ultimoIpValido: string;
-  quantidadeHosts: number;
-  classeIp: string;
+  ipInformado: string; mascaraSubrede: string; prefixoCidr: string;
+  enderecoRede: string; enderecoBroadcast: string; primeiroIpValido: string;
+  ultimoIpValido: string; quantidadeHosts: number; classeIp: string;
+  blocos: BlocoRedeDTO[];
 }
 
 @Component({
@@ -23,33 +23,19 @@ interface RedeDTO {
   styleUrl: './app.css'
 })
 export class App {
-
-  ipDigitado: string = '';
-  mascaraDigitada: string = '';
-
-  resultadoDaAPI: RedeDTO | null = null;
-  erroMsg: string = '';
+  ipDigitado: string = ''; mascaraDigitada: string = '';
+  resultadoDaAPI: RedeDTO | null = null; erroMsg: string = '';
 
   constructor(private http: HttpClient) {}
 
   calcularRede() {
-    if (!this.ipDigitado || !this.mascaraDigitada) {
-      this.erroMsg = 'Por favor, preencha os dois campos!';
-      return;
-    }
-
+    if (!this.ipDigitado || !this.mascaraDigitada) { this.erroMsg = 'Preencha os campos!'; return; }
     this.erroMsg = '';
     const url = `http://localhost:8080/api/calculadora/calcular?ip=${this.ipDigitado}&mascara=${encodeURIComponent(this.mascaraDigitada)}`;
 
     this.http.get<RedeDTO>(url).subscribe({
-      next: (dados) => {
-        this.resultadoDaAPI = dados;
-      },
-      error: (erro) => {
-        console.error(erro);
-        this.erroMsg = 'Valores inválidos ou servidor offline. Verifique o formato digitado.';
-        this.resultadoDaAPI = null;
-      }
+      next: (dados) => { this.resultadoDaAPI = dados; },
+      error: () => { this.erroMsg = 'Valores inválidos ou servidor offline.'; this.resultadoDaAPI = null; }
     });
   }
 }
